b556285d905c3ec51908b60d3c5ab43106774bfb
FALKON <campossoriao1@gmail.com>
2024-02-19 23:55:39 +0100

    Agregar archivos de proyecto.

---
+++ Act2Riojas/Bola.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Act2Riojas
+{
+    class Bola
+    {
+        // Miembro de la clase Bola: peso (en kilogramos)
+        private double peso;
+
+        // Constructor de la clase Bola
+        public Bola(double peso)
+        {
+            this.peso = peso;
+        }
+
+        // Método de la clase Bola para calcular la distancia recorrida
+        public double CalcularDistancia(double fuerza)
+        {
+            // Declaración de una variable local para la aceleración debido a la gravedad (en m/s^2)
+            double gravedad = 9.8;
+
+            // Cálculo de la aceleración usando la fuerza y el peso
+            double aceleracion = fuerza / peso;
+
+            // Cálculo de la distancia utilizando la fórmula de movimiento uniformemente acelerado
+            double distancia = (fuerza / peso) * (fuerza / (2 * gravedad));
+
+            // Retorno de la distancia calculada
+            return distancia;
+        }
+    }
+}
+
---
+++ Act2Riojas/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Act2Riojas
+{
+    class Program
+    {
+        
+       
+            static void Main(string[] args)
+            {
+                try
+                {
+                    // Creación de un objeto de la clase Bola con un peso de 1 kilogramo
+                    Bola miBola = new Bola(1.0);
+                    Console.WriteLine("Este programa simula la distancia que recorerria una bola que pesa 1k si es golpeada por una fuerza en newtons");
+
+                    // Solicitar al usuario que ingrese la fuerza en Newton
+                    Console.Write("Ingrese la fuerza en Newton: ");
+                    double fuerza = Convert.ToDouble(Console.ReadLine());
+
+                    // Calcular la distancia recorrida por la bola
+                    double distanciaRecorrida = miBola.CalcularDistancia(fuerza);
+
+                    // Mostrar la distancia recorrida por la bola
+                    Console.WriteLine("La bola recorrerá una distancia de " + distanciaRecorrida.ToString("0.00") + " metros.");
+                }
+                catch (FormatException)
+                {
+                    // Captura y manejo de la excepción en caso de que el usuario ingrese un valor no válido
+                    Console.WriteLine("Error: Por favor, ingrese un valor numérico válido.");
+                }
+                catch (Exception ex)
+                {
+                    // Captura y manejo de cualquier otra excepción no prevista
+                    Console.WriteLine("Error: " + ex.Message);
+                }
+            }
+        }
+    }
